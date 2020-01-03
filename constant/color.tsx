afea66764c626dbf327704d9393bd068bf6cdc92
Untung Andryanto (ID) <untung.andryanto@homecredit.co.id>
2020-01-04 03:02:34 +0700

    init

---
+++ constant/color.tsx
@@ -0,0 +1,86 @@
+const colorPallete = [
+  {
+    color: "black",
+    hexadecimal: "#000000",
+    rgba: "rgba(0, 0, 0, 1)",
+  },
+  {
+    color: "darkBlue",
+    hexadecimal: "#1D2B53",
+    rgba: "rgba(29, 43, 83, 1)",
+  },
+
+  {
+    color: "darkPurple",
+    hexadecimal: "#7E2553",
+    rgba: "rgba(126, 37, 83, 1)",
+  },
+
+  {
+    color: "darkGreen",
+    hexadecimal: "#008751",
+    rgba: "rgba(0, 135, 81, 1)",
+  },
+  {
+    color: "brown",
+    hexadecimal: "#AB5236",
+    rgba: "rgba(171, 82, 54, 1)",
+  },
+  {
+    color: "darkGray",
+    hexadecimal: "#5F574F",
+    rgba: "rgba(95, 87, 79, 1)",
+  },
+  {
+    color: "lightGray",
+    hexadecimal: "#C2C3C7",
+    rgba: "rgba(194, 195, 199, 1)",
+  },
+  {
+    color: "white",
+    hexadecimal: "#FFF1E8",
+    rgba: "rgba(255, 241, 232, 1)",
+  },
+  {
+    color: "red",
+    hexadecimal: "#FF004D",
+    rgba: "rgba(255, 0, 77, 1)",
+  },
+  {
+    color: "orange",
+    hexadecimal: "#FFA300",
+    rgba: "rgba(255, 163, 0, 1)",
+  },
+  {
+    color: "yellow",
+    hexadecimal: "#FFEC27",
+    rgba: "rgba(255, 236, 39, 1)",
+  },
+  {
+    color: "green",
+    hexadecimal: "#00E436",
+    rgba: "rgba(0, 228, 54, 1)",
+  },
+  {
+    color: "blue",
+    hexadecimal: "#29ADFF",
+    rgba: "rgba(41, 173, 255, 1)",
+  },
+  {
+    color: "indigo",
+    hexadecimal: "#83769C",
+    rgba: "rgba(131, 118, 156, 1)",
+  },
+  {
+    color: "pink",
+    hexadecimal: "#FF77A8",
+    rgba: "rgba(255, 119, 168, 1)",
+  },
+  {
+    color: "peach",
+    hexadecimal: "#FFCCAA",
+    rgba: "rgba(255, 204, 170, 1)",
+  },
+];
+
+export { colorPallete };
